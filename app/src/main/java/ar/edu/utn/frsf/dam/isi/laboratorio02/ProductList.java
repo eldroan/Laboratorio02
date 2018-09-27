76a130447880648919d35d7c211602b61d341811
Germán Ignacio Bernal <bernalgermanignacio@gmail.com>
2018-09-27 20:39:04 -0300

    Se realizo el item e del punto 2

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ProductList.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ProductList.java
@@ -4,16 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.*;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Producto;
 
 public class ProductList extends AppCompatActivity {
 
     private Spinner cmbProductosCategoria;
+    private TextView categoria;
+    private ListView listaProductos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,13 +27,49 @@ public class ProductList extends AppCompatActivity {
         ProductoRepository repo = new ProductoRepository();
 
         List<Categoria> categorias = repo.getCategorias();
-        List<String> catString = new ArrayList<String>();
+        final List<String> catString = new ArrayList<String>();
         for(Categoria c : categorias){
             catString.add(c.toString());
         }
-        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(ProductList.this, android.R.layout.simple_spinner_item, catString);
+        final List<Producto> prodList = repo.getLista();
+        final List<String> prodString = new ArrayList<String>();
+
+
+        final ArrayAdapter<String> adaptador = new ArrayAdapter<String>(ProductList.this, android.R.layout.simple_spinner_item, catString);
+        final ArrayAdapter<String> adaptadorListView = new ArrayAdapter<String>(ProductList.this, android.R.layout.simple_list_item_1, prodString);
+
+
+
         cmbProductosCategoria = (Spinner) findViewById(R.id.cmbProductosCategoria);
+        categoria = (TextView) findViewById(R.id.textView2);
+        listaProductos = (ListView) findViewById(R.id.lstProductos);
+
+
+
         cmbProductosCategoria.setAdapter(adaptador);
+        cmbProductosCategoria.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener()
+                {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view,int position, long id) {
+                        prodString.clear();
+                        listaProductos.setAdapter(adaptadorListView);
+                        for(Producto p : prodList){
+                            if(p.getCategoria().toString().equals(adapterView.getItemAtPosition(position).toString()))
+                                prodString.add(p.toString());
+                        }
+
+
+                    }
+                    public void onNothingSelected(AdapterView<?> parent)
+                    {
+
+                    }
+                }
+        );
     }
+
+
+
 
 }
