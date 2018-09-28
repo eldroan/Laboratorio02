d336560efb39ed5e2bb1986ddbc9726110f340ed
Germán Ignacio Bernal <bernalgermanignacio@gmail.com>
2018-09-27 22:12:14 -0300

    Se finaliza el punto 2

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ProductList.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ProductList.java
@@ -3,12 +3,15 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.*;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.List;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
@@ -28,11 +31,9 @@ public class ProductList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
 
-        Bundle myBundle = getIntent().getExtras();
-        int nuevopedido = 0;
-        if(myBundle.containsKey("NUEVO_PEDIDO")){
-            nuevopedido = myBundle.getInt("NUEVO_PEDIDO");
-        }
+        final Intent myIntent = getIntent();
+        //Bundle myBundle = myIntent.getExtras();
+        int nuevopedido = myIntent.getIntExtra("NUEVO_PEDIDO",0);
 
 
         ProductoRepository repo = new ProductoRepository();
@@ -47,15 +48,48 @@ public class ProductList extends AppCompatActivity {
 
 
         final ArrayAdapter<String> adaptador = new ArrayAdapter<String>(ProductList.this, android.R.layout.simple_spinner_item, catString);
-        final ArrayAdapter<String> adaptadorListView = new ArrayAdapter<String>(ProductList.this, android.R.layout.simple_list_item_1, prodString);
+
+        final ArrayAdapter<String> adaptadorListView = new ArrayAdapter<String>(ProductList.this, android.R.layout.simple_list_item_single_choice, prodString);
 
 
 
         cmbProductosCategoria = (Spinner) findViewById(R.id.cmbProductosCategoria);
         categoria = (TextView) findViewById(R.id.textView2);
         listaProductos = (ListView) findViewById(R.id.lstProductos);
+        listaProductos.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        //listaProductos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //    @Override
+        //    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //        listaProductos.setSelection(position);
+        //    }
+        //});
         quantity = (EditText) findViewById(R.id.editTextCantidad);
         agregarPedido = (Button) findViewById(R.id.buttonAgregarPedido);
+        final HashMap<Integer,Integer> listPosToProdId = new HashMap<Integer,Integer>();
+        agregarPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Integer cant = Integer.parseInt(quantity.getText().toString());
+                    myIntent.putExtra("cantidad",cant);
+                    int selectedItemId = listaProductos.getCheckedItemPosition();
+                    Integer selectedProductId = listPosToProdId.get(selectedItemId);
+                    if(selectedProductId == null)
+                        throw new Exception("Se rompio todo");
+
+                    myIntent.putExtra("idProducto",selectedProductId);
+                    Log.println(Log.WARN,"LOCO","MENSAJE " +myIntent.getExtras().toString());
+                   // Toast.makeText(ProductList.this,myIntent.getExtras().toString(),Toast.LENGTH_SHORT).show();
+
+                }catch(Exception ex){
+                    Log.println(Log.ERROR,"LOCO","MENSAJE " );
+                    Toast.makeText(ProductList.this,"Que toast re loco",Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
 
         if(nuevopedido != 1){
             agregarPedido.setEnabled(false);
@@ -70,9 +104,13 @@ public class ProductList extends AppCompatActivity {
                     public void onItemSelected(AdapterView<?> adapterView, View view,int position, long id) {
                         prodString.clear();
                         listaProductos.setAdapter(adaptadorListView);
+                        int index =0;
                         for(Producto p : prodList){
-                            if(p.getCategoria().toString().equals(adapterView.getItemAtPosition(position).toString()))
+                            if(p.getCategoria().toString().equals(adapterView.getItemAtPosition(position).toString())){
                                 prodString.add(p.toString());
+                                listPosToProdId.put(index,p.getId());
+                                index++;
+                            }
                         }
 
 
