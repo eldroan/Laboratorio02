c57931277859365928e3494d5e9b15ad2b6ef359
lemarillo <leandroamarillo@outlook.com>
2018-10-18 18:23:45 -0300

    Se finaliza el punto 5

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoAdapter.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PedidoAdapter.java
@@ -1,6 +1,7 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,6 +12,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.BreakIterator;
 import java.util.List;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
@@ -33,7 +35,7 @@ public class PedidoAdapter extends ArrayAdapter<Pedido> {
 
         if(convertView == null){
             LayoutInflater inflater = LayoutInflater.from(this.ctx);
-            View fila = inflater.inflate(R.layout.fila_historial,parent,false);
+             convertView = inflater.inflate(R.layout.fila_historial,parent,false);
             holder = new PedidoHolder();
 
             holder.textViewContacto = (TextView) convertView.findViewById(R.id.textViewContacto);
@@ -43,6 +45,20 @@ public class PedidoAdapter extends ArrayAdapter<Pedido> {
             holder.textViewEstado = (TextView) convertView.findViewById(R.id.textViewEstado);
             holder.imageView = (ImageView) convertView.findViewById(R.id.imageView);
             holder.buttonCancelar = (Button) convertView.findViewById(R.id.buttonCancelar);
+            holder.buttonCancelar.setOnClickListener(null);
+            holder.buttonCancelar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int indice = (int) v.getTag();
+                    Pedido pedidoSeleccionado = listaPedidos.get(indice);
+                    if(pedidoSeleccionado.getEstado().equals(Pedido.Estado.REALIZADO) || pedidoSeleccionado.getEstado().equals(Pedido.Estado.ACEPTADO) || pedidoSeleccionado.getEstado().equals(Pedido.Estado.EN_PREPARACION)){
+                        pedidoSeleccionado.setEstado(Pedido.Estado.CANCELADO);
+                        PedidoAdapter.this.notifyDataSetChanged();
+                        return;
+                    }
+
+                }
+            });
             holder.buttonDetalle = (Button) convertView.findViewById(R.id.buttonVerDetalle);
             convertView.setTag(holder);
         } else {
@@ -56,9 +72,32 @@ public class PedidoAdapter extends ArrayAdapter<Pedido> {
         holder.textViewItems.setText(pedido.getDetalle().size());
         holder.textViewAPagar.setText(pedido.total().toString());
         holder.textViewEstado.setText(pedido.getEstado().toString());
-        holder.imageView.setImageResource(pedido.getRetirar()? R.mipmap.imageRetira : R.mipmap.imageEnvio);
+        holder.imageView.setImageResource(pedido.getRetirar()? R.mipmap.retira : R.mipmap.envio);
 
-        return super.getView(position, convertView, parent);
+        switch(pedido.getEstado()){
+            case LISTO:
+                holder.textViewEstado.setTextColor(Color.DKGRAY);
+                break;
+            case ENTREGADO:
+                holder.textViewEstado.setTextColor(Color.BLUE);
+                break;
+            case CANCELADO:
+            case RECHAZADO:
+                holder.textViewEstado.setTextColor(Color.RED);
+                break;
+            case ACEPTADO:
+                holder.textViewEstado.setTextColor(Color.GREEN);
+                break;
+            case EN_PREPARACION:
+                holder.textViewEstado.setTextColor(Color.MAGENTA);
+                break;
+            case REALIZADO:
+                holder.textViewEstado.setTextColor(Color.BLUE);
+                break;
+
+        }
+
+        return convertView;
     }
 
     private static class PedidoHolder{
