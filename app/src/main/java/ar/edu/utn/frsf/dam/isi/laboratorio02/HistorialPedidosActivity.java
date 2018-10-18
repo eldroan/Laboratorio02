e55370edd3129800eb4e17e4de622ca14a28e24d
lemarillo <leandroamarillo@outlook.com>
2018-10-18 20:12:21 -0300

    Se conecta el adapter y se agrega el intent para acceder a la activity desde main

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/HistorialPedidosActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/HistorialPedidosActivity.java
@@ -8,6 +8,7 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
 
 public class HistorialPedidosActivity extends AppCompatActivity implements View.OnClickListener{
@@ -26,6 +27,10 @@ public class HistorialPedidosActivity extends AppCompatActivity implements View.
         bttnHistorialNuevo = (Button) findViewById(R.id.bttnHistorialNuevo);
         bttnHistorialNuevo.setOnClickListener(this);
         bttnHistorialMenu.setOnClickListener(this);
+        PedidoRepository pr = new PedidoRepository();
+
+        PedidoAdapter pa = new PedidoAdapter(this,pr.getLista());
+        lstHistorialPedido.setAdapter(pa);
 
         lstHistorialPedido.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -51,11 +56,11 @@ public class HistorialPedidosActivity extends AppCompatActivity implements View.
         Intent i;
         switch (v.getId()){
             case R.id.bttnHistorialMenu:
-                i = new Intent(HistorialPedidosActivity.this, MainActivity.class);
+                i = new Intent(HistorialPedidosActivity.this, AltaProductoActivity.class);
                 startActivity(i);
                 break;
             case R.id.bttnHistorialNuevo:
-                i = new Intent(HistorialPedidosActivity.this, AltaProductoActivity.class);
+                i = new Intent(HistorialPedidosActivity.this, MainActivity.class);
                 i.putExtra("ID_PEDIDO",-1);
                 startActivity(i);
                 break;
