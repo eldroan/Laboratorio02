e83d61addfb2c563544105123531edf3568f7785
Luciano Arrúa <lucianorarrua@gmail.com>
2018-12-01 04:14:33 -0300

    Finaliza Lab04-req05 y Lab04-req06

---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/dao/CategoriaDao.java
@@ -0,0 +1,31 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
+
+@Dao
+public interface CategoriaDao {
+
+    @Query("SELECT * FROM Categoria")
+    List<Categoria> getAll();
+
+    @Query("SELECT * FROM Categoria WHERE id_categoria = :id")
+    Categoria getForId(Integer id);
+
+    @Insert
+    long insert(Categoria cat);
+
+    @Update
+    void update(Categoria cat);
+
+    @Delete
+    void delete(Categoria cat);
+
+}
